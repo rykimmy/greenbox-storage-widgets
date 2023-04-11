9fa42515152fc0ab1fa54b4bf8502885ab2e5dd8
Ryan <rkim20@choate.edu>
2023-04-10 21:58:24 -0400

    uploading working code for repo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,436 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import customers from './customers.json';
+import { Chart } from 'chart.js/auto'
+import { Doughnut, Bar } from 'react-chartjs-2';
+import { Box, Table, TableCell, TableBody, TableContainer, TableHead, TableRow, Paper, Typography } from '@mui/material'
 
 function App() {
+
+  // STATES
+  const [schoolState, setSchoolState] = useState("All Schools");
+  const [customerData, setCustomerData] = useState(customers);
+  const [conversionData, setConversionData] = useState([]);
+  const [revenueData, setRevenueData] = useState([]);
+  const [pickupData, setPickupData] = useState([]);
+  const [conversionRate, setConversionRate] = useState('')
+
+  // SCHOOL SELECT
+  const SchoolSelect = (props) => {
+    return (
+      <select
+        value={props.school}
+        onChange={(event) => setSchoolState(event.target.value)}
+      >
+        <option>All Schools</option>
+        <option>Brown</option>
+        <option>Columbia</option>
+        <option>Cornell</option>
+        <option>Dartmouth</option>
+        <option>Harvard</option>
+        <option>Princeton</option>
+        <option>UPenn</option>
+        <option>Yale</option>
+      </select>
+    );
+  };
+
+  // MONTHLY REV CALCULATION
+  function calculateRevenueByMonth(data, schoolState) {
+    // Initialize an empty array to store the revenue data
+    const revenueByMonth = [];
+
+    // Iterate over the customer data and calculate the projected revenue for each month
+    data.forEach(customer => {
+      if (customer.school === schoolState || schoolState === "All Schools") {
+        const pickupDate = new Date(customer.pickupDate);
+        const returnDate = new Date(customer.returnDate);
+
+        // Calculate the number of months between the pickup and return dates
+        const months = (returnDate.getFullYear() - pickupDate.getFullYear()) * 12 + (returnDate.getMonth() - pickupDate.getMonth());
+
+        // Calculate the revenue per month
+        const revenuePerMonth = customer.monthlyCost;
+
+        // Iterate over each month and add the revenue to the array
+        for (let i = 0; i < months; i++) {
+          const month = new Date(pickupDate.setMonth(pickupDate.getMonth()+1));
+          const monthName = month.toLocaleString('default', { month: 'short' });
+          const year = month.getFullYear();
+          const label = `${monthName} ${year}`;
+          const revenue = revenueByMonth.find(item => item.label === label);
+
+          if (revenue) {
+            revenue.value += revenuePerMonth;
+          } else {
+            revenueByMonth.push({ label, value: revenuePerMonth });
+          }
+        }
+      }
+    });
+
+    // Return the revenue data
+    return revenueByMonth;
+  }
+
+  // CONVERSION DATA
+  function calculateConversion(data, schoolState) {
+    // Initialize variables for conversion
+    let conversionRate = 0;
+    let accounts = 0;
+    let orders = 0;
+
+    // Iterate over the customer data and calculate the conversion rate
+    data.forEach(customer => {
+      if (customer.school === schoolState || schoolState === "All Schools") {
+        accounts = accounts + 1;
+        if (customer.hasOwnProperty("numItems")) {
+          orders = orders + 1;
+        }
+      }
+    });
+
+    conversionRate = orders / accounts * 100;
+    const conversionData = [orders, accounts - orders, conversionRate];
+
+    return conversionData;
+  }
+
+  // PICKUP DATA
+  function calculatePickupData(data, schoolState) {
+    // Initialize an empty array to store the revenue data
+    const pickupInfo = [];
+
+    // Iterate over the customer data and calculate the projected revenue for each month
+    data.forEach(customer => {
+      if (customer.school === schoolState || schoolState === "All Schools") {
+
+        const pickupDate = new Date(customer.pickupDate);
+        const returnDate = new Date(customer.returnDate);
+
+        // Calculate the number of months between the pickup and return dates
+        const months = (returnDate.getFullYear() - pickupDate.getFullYear()) * 12 + (returnDate.getMonth() - pickupDate.getMonth());
+
+        // Calculate the revenue per month
+        const items = customer.numItems;
+
+        // Iterate over each month and add the revenue to the array
+        for (let i = 0; i < months; i++) {
+          const dateUTC = new Date(pickupDate.setMonth(pickupDate.getMonth() + 1));
+          const day = dateUTC.getDate();
+          const year = dateUTC.getFullYear();
+          const month = dateUTC.getMonth();
+          const date = `${month}/${day}/${year}`;
+          const pickupItems = pickupInfo.find(item => item.date === date);
+
+          if (pickupItems) {
+            pickupItems.items += items;
+            pickupItems.customers += 1;
+          } else {
+            pickupInfo.push({ date, items, customers: 1 });
+          }
+        }
+      }
+    });
+    var pickupSorted = pickupInfo.sort((a,b) => a.date[0]-b.date[0]);
+    // Return the pickup data
+    return pickupSorted;
+  }
+
+  useEffect(() => {
+    setPickupData(calculatePickupData(customerData, schoolState));
+    setConversionData(calculateConversion(customerData, schoolState));
+    setRevenueData(calculateRevenueByMonth(customerData, schoolState));
+
+  }, [customerData, schoolState]);
+
+  useEffect(() => {
+    setConversionRate(Math.floor(conversionData[0]/(conversionData[0] + conversionData[1]) * 100));
+  }, [conversionData]);
+
+
+  const conversionChartData = {
+    labels: ["Reservations", "Accounts without Reservations"],
+    datasets: [{
+      data: [conversionData[0], conversionData[1]],
+      backgroundColor: [
+        '#307a42',
+        '#eee'
+      ]
+    }]
+  }
+
+  const revenueChartData = {
+    labels: revenueData.map(function (label) { return label.label }),
+    datasets: [{
+      label: "Monthly Revenue",
+      data: revenueData.map(function (value) { return value.value }),
+      backgroundColor: ['#307a42']
+    }]
+
+  };
+
+
+  console.log(revenueChartData)
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Typography className='headingTypo' variant="h3" mt={4}>Greenbox Storage Widgets</Typography>
+
+
+      {/* <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js"></script> */}
+      <Box className='chartDiv'>
+      <SchoolSelect
+        school={schoolState}
+      />
+        <Box className='chartBox'>
+          <Doughnut data={conversionChartData} options={{
+            responsive: true,
+            maintainAspectRatio: true,
+          }} />
+          <Typography>Conversion Rate: {conversionRate}%</Typography>
+        </Box>
+
+        <Box className='chartBox'>
+          <Bar data={revenueChartData} />
+        </Box>
+
+
+        <Box className='chartBox'>
+          <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 300 }} aria-label="simple table">
+              <TableHead>
+                <TableRow>
+                  <TableCell align="center">Date</TableCell>
+                  <TableCell align="center">Customers</TableCell>
+                  <TableCell align="center">Items</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {pickupData.map((row) => (
+                  <TableRow
+                    key={row.name}
+                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                  >
+                    <TableCell align="center" component="th" scope="row">
+                      {row.date}
+                    </TableCell>
+                    <TableCell align="center">{row.customers}</TableCell>
+                    <TableCell align="center">{row.items}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Box>
+      </Box>
+
+
+      {/* <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js"></script> */}
+
+
+
+
+      {/* <RevenueData
+    //     posts={customerData}
+    //     school={schoolState}
+    //   />
+
+    //   <ConversionData
+    //     posts={customerData}
+    //     school={schoolState}
+    //   />
+
+    //   <PickupData
+    //     posts={customerData}
+    //     school={schoolState}
+    //   /> */}
     </div>
   );
 }
+
+// PICKUP DATE
+// function PickupData(props) {
+
+//   function calculatePickupData(data, schoolState) {
+//     // Initialize an empty array to store the revenue data
+//     const pickupInfo = [];
+
+//     // Iterate over the customer data and calculate the projected revenue for each month
+//     data.forEach(customer => {
+//       if (customer.school === schoolState || schoolState === "All Schools") {
+
+//         const pickupDate = new Date(customer.pickupDate);
+//         const returnDate = new Date(customer.returnDate);
+
+//         // Calculate the number of months between the pickup and return dates
+//         const months = (returnDate.getFullYear() - pickupDate.getFullYear()) * 12 + (returnDate.getMonth() - pickupDate.getMonth());
+
+//         // Calculate the revenue per month
+//         const items = customer.numItems;
+
+//         // Iterate over each month and add the revenue to the array
+//         for (let i = 0; i < months; i++) {
+//           const dateUTC = new Date(pickupDate.setMonth(pickupDate.getMonth() + 1));
+//           const day = dateUTC.getDate();
+//           const year = dateUTC.getFullYear();
+//           const month = dateUTC.getMonth();
+//           const date = `${month}/${day}/${year}`;
+//           const pickupItems = pickupInfo.find(item => item.date === date);
+
+//           if (pickupItems) {
+//             pickupItems.items += items;
+//             pickupItems.customers += 1;
+//           } else {
+//             pickupInfo.push({ date, items, customers: 1 });
+//           }
+//         }
+//       }
+//     });
+
+//     // Return the revenue data
+//     return pickupInfo;
+//   }
+
+//   const [pickupData, setPickupData] = useState([]);
+
+//   useEffect(() => {
+//     setPickupData(calculatePickupData(props.posts, props.school));
+//   }, [props.school]);
+
+// return (
+//   <table>
+//     <thead>
+//       <tr>
+//         <th>Pickup Date</th>
+//         <th>Customers</th>
+//         <th>Items</th>
+//       </tr>
+//     </thead>
+//     <tbody>
+//       {pickupData.map(customer => (
+//         <tr key={customer.id}>
+//           <td>{customer.date}</td>
+//           <td>{customer.customers}</td>
+//           <td>{customer.items}</td>
+//         </tr>
+//       ))}
+//     </tbody>
+//   </table>
+// );
+// };
+
+// CONVERSION DATA
+// function ConversionData(props) {
+
+//   function calculateConversion(data, schoolState) {
+//     // Initialize variables for conversion
+//     let conversionRate = 0;
+//     let accounts = 0;
+//     let orders = 0;
+
+//     // Iterate over the customer data and calculate the conversion rate
+//     data.forEach(customer => {
+//       if (customer.school === schoolState || schoolState === "All Schools") {
+//         accounts = accounts + 1;
+//         if (customer.hasOwnProperty("numItems")) {
+//           orders = orders + 1;
+//         }
+//       }
+//     });
+
+//     conversionRate = orders / accounts;
+//     const conversionData = [accounts, orders, conversionRate];
+
+//     return conversionData;
+//   }
+
+//   return (
+//     <table>
+//       <thead>
+//         <tr>
+//           <th>Accounts Made</th>
+//           <th>Reservations</th>
+//           <th>Conversion Rate</th>
+//         </tr>
+//       </thead>
+//       <tbody>
+//         <tr>
+//           <td>{calculateConversion(props.posts, props.school)[0]}</td>
+//           <td>{calculateConversion(props.posts, props.school)[1]}</td>
+//           <td>{calculateConversion(props.posts, props.school)[2]}</td>
+//         </tr>
+//       </tbody>
+//     </table>
+//   );
+// };
+
+// REVENUE DATA
+// function RevenueData(props) {
+
+//   // MONTHLY REV CALCULATION
+//   function calculateRevenueByMonth(data, schoolState) {
+//     // Initialize an empty array to store the revenue data
+//     const revenueByMonth = [];
+
+//     // Iterate over the customer data and calculate the projected revenue for each month
+//     data.forEach(customer => {
+//       if (customer.school === schoolState || schoolState === "All Schools") {
+//         const pickupDate = new Date(customer.pickupDate);
+//         const returnDate = new Date(customer.returnDate);
+
+//         // Calculate the number of months between the pickup and return dates
+//         const months = (returnDate.getFullYear() - pickupDate.getFullYear()) * 12 + (returnDate.getMonth() - pickupDate.getMonth());
+
+//         // Calculate the revenue per month
+//         const revenuePerMonth = customer.monthlyCost;
+
+//         // Iterate over each month and add the revenue to the array
+//         for (let i = 0; i < months; i++) {
+//           const month = new Date(pickupDate.setMonth(pickupDate.getMonth() + 1));
+//           const monthName = month.toLocaleString('default', { month: 'short' });
+//           const year = month.getFullYear();
+//           const label = `${monthName} ${year}`;
+//           const revenue = revenueByMonth.find(item => item.label === label);
+
+//           if (revenue) {
+//             revenue.value += revenuePerMonth;
+//           } else {
+//             revenueByMonth.push({ label, value: revenuePerMonth });
+//           }
+//         }
+//       }
+//     });
+
+//     // Return the revenue data
+//     return revenueByMonth;
+//   }
+
+//   const [revenueData, setRevenueData] = useState([]);
+
+//   useEffect(() => {
+//     setRevenueData(calculateRevenueByMonth(props.posts, props.school));
+//   }, [props.school]);
+
+//   return (
+//     <table>
+//       <thead>
+//         <tr>
+//           <th>Month</th>
+//           <th>Revenue</th>
+//         </tr>
+//       </thead>
+//       <tbody>
+//         {revenueData.map(customer => (
+//           <tr key={customer.id}>
+//             <td>{customer.label}</td>
+//             <td>{customer.value}</td>
+//           </tr>
+//         ))}
+//       </tbody>
+//     </table>
+//   );
+// }
 
 export default App;
